8f09d10089ed73091674ed759b290c1172343098
Leo Blanchette <leo@symbiostock.com>
2013-04-02 23:53:24 -1000

    Another Page Template

---
+++ page-fullwidth.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Template Name: Page - Full Width
+*/
+/**
+ * The template for displaying page full width.
+ *
+ * @package symbiostock
+ * @since symbiostock 1.0
+ */
+get_header(); ?>
+		<div class="home row-fluid">
+        
+              <div id="primary" class="content-area span12">  
+               
+                <div id="content" class="site-content" role="main">
+    
+                    <?php while ( have_posts() ) : the_post(); ?>
+    
+                        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                            <div class="entry-content">
+                                <?php the_content(); ?>
+                                <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'symbiostock' ), 'after' => '</div>' ) ); ?>
+                                <?php edit_post_link( __( 'Edit', 'symbiostock' ), '<span class="edit-link">', '</span>' ); ?>
+                            </div><!-- .entry-content -->
+                        </article><!-- #post-<?php the_ID(); ?> -->
+    
+                        <?php comments_template( '', true ); ?>
+    
+                    <?php endwhile; // end of the loop. ?>
+    
+                </div><!-- #content .site-content -->
+            
+            
+          
+            <?php dynamic_sidebar( 'Featured Posts (Below Content)' ) ?>
+         
+    		</div><!-- #primary .content-area -->
+
+        </div>        
+        
+    	
+<?php get_footer(); ?>
