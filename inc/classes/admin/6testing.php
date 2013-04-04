ebbf0d62ef7a29e2a04f4127c7246996ad66010e
Leo Blanchette <leo@symbiostock.com>
2013-04-04 00:22:56 -1000

    Added Imagic Test

--- inc/classes/admin/6testing.php
+++ inc/classes/admin/6testing.php
@@ -1,4 +1,13 @@
+<?php
+if (!extension_loaded('imagick')){
+   		echo '<h3>Imagick is not installed on this server. :(</h3>';
+	}else {		
+		echo '<h3>Imagick is installed on this server!!! Good times ahead!</h3>';	
+	}
 
+
+?>
+<hr />
 <div class="">
 <p>This is a temporary page while Symbiostock is in its early testing / development phase.</p>
 <br />
